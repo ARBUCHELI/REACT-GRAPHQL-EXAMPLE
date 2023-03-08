4bce313a6364fbf7db3df9e5f32ffdcc5eafe6d9
ARBUCHELI <radio_seattle@hotmail.com>
2023-03-07 22:00:53 -0500

    creating NavButtons and SearchBox

--- src/Query.js
+++ src/Query.js
@@ -1,25 +1,42 @@
-const githubQuery = {
-    query: `
-    {
-        viewer {
-            name
-        }
-        search(query: "user:ARBUCHELI sort:updated-desc", type: REPOSITORY, first: 10) {
-            nodes {
-                ... on Repository {
-                    name
-                    description
-                    id
-                    url
-                    viewerSubscription 
-                    licenseInfo {
-                        spdxId
+const githubQuery = (
+    pageCount, 
+    queryString, 
+    paginationKeyword, 
+    paginationString
+) => {
+    return {
+        query: `
+        {
+            viewer {
+                name
+            }
+            search(query: "${queryString} user:ARBUCHELI sort:updated-desc", type: REPOSITORY, ${paginationKeyword}: ${pageCount}, ${paginationString}) {
+                repositoryCount
+                edges {
+                    cursor
+                    node {
+                        ... on Repository {
+                            name
+                            description
+                            id
+                            url
+                            viewerSubscription 
+                            licenseInfo {
+                                spdxId
+                            }
+                        }
                     }
+                }
+                pageInfo {
+                    startCursor
+                    endCursor
+                    hasNextPage
+                    hasPreviousPage
                 }
             }
         }
-    }
-  `,
-  };
+    `,
+    };    
+};
 
 export default githubQuery;
